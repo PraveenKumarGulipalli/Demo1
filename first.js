15758e3b882535eeb2a66d3920cb526f787348fe
PraveenKumarGulipalli <110034594+PraveenKumarGulipalli@users.noreply.github.com>
2022-09-14 14:56:51 +0530

    Update first.js

--- first.js
+++ first.js
@@ -1,14 +1,8 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const app=require("express")();
+const PORT=process.env.PORT || 3000;
+app.get("",(req,res)=>{
+    res.send("hello world");
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, ()=>{
+    console.log('app up at port ${PORT}');
 });
